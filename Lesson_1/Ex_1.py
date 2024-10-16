d1014c4f43cd03783ee84746335409fca42abfe8
KurochkinAndrew <120648391+KurochkinAndrew@users.noreply.github.com>
2024-10-16 22:27:17 +1000

    Update Ex_1.py

--- Lesson_1/Ex_1.py
+++ Lesson_1/Ex_1.py
@@ -1,29 +1,27 @@
 import requests
+import logging
 
 
-url_template = 'https://wttr.in/{}?MnqT'
-headers = {
-    'User-Agent': 'curl',
-    'Accept-Language': 'ru-Ru'
-}
+logger = logging.getLogger('app_logger')
+url_template = 'https://wttr.in/{}'
+places_list = ['svo', 'Лондон', 'Череповец']
 
-url_svo = url_template.format('/svo')
+def get_weather():
+    payload = {
+        'qMTn': '',
+        'lang': 'ru'
+    }
+    for place in places_list:
+        response = None
+        url = url_template.format(place)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
 
-response_svo = requests.get(url_svo, headers=headers)
-response_svo.raise_for_status()
+        print(response.text)
 
-print(response_svo.text)
+def main():
+    get_weather()
 
-url_london = url_template.format('/London')
 
-response_london = requests.get(url_london, headers=headers)
-response_london.raise_for_status()
-
-print(response_london.text)
-
-url_cherepovets = url_template.format("Череповец")
-
-response_cherepovets = requests.get(url_cherepovets, headers=headers)
-response_cherepovets.raise_for_status()
-
-print(response_cherepovets.text)
+if __name__ == '__main__':
+    main()
