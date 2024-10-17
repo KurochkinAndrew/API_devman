ef63d38bc3a6293bcb0511f984c8574d69a39efe
KurochkinAndrew <120648391+KurochkinAndrew@users.noreply.github.com>
2024-10-17 20:35:14 +1000

    Update Ex_1.py

--- Lesson_1/Ex_1.py
+++ Lesson_1/Ex_1.py
@@ -1,27 +1,25 @@
 import requests
-import logging
 
 
-logger = logging.getLogger('app_logger')
-url_template = 'https://wttr.in/{}'
-places_list = ['svo', 'Лондон', 'Череповец']
-
-def get_weather():
+def get_weather(place_name):
     payload = {
         'qMTn': '',
         'lang': 'ru'
     }
-    for place in places_list:
-        response = None
-        url = url_template.format(place)
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
+    url_template = 'https://wttr.in/{}'
 
-        print(response.text)
+    url = url_template.format(place_name)
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+
+    return response.text
+
 
 def main():
-    get_weather()
+    places = ['svo', 'Лондон', 'Череповец']
 
+    for place in places:
+        print(get_weather(place))
 
 if __name__ == '__main__':
     main()
