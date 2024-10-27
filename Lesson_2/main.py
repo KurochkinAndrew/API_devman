4b1251330c498dfd9056c0b3fbc8a1e4e0992dae
KurochkinAndrew <120648391+KurochkinAndrew@users.noreply.github.com>
2024-10-27 13:52:59 +1000

    Update1 main.py

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -4,10 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv()
-
-
-def shorten_link(token, user_input):
+def get_shorten_link(token, user_input):
     url = "https://api.vk.com/method/utils.getShortLink"
     payload = {
         'url': user_input
@@ -19,10 +16,13 @@ def shorten_link(token, user_input):
     response = requests.post(url, data=payload, params=headers)
     response.raise_for_status()
 
-    return response.json()
+    try:
+        return response.json()['response']['short_url']
+    except KeyError:
+        return 'Вы ввели неправильную ссылку'
 
 
-def count_clicks(token, link):
+def get_count_clicks(token, link):
     url = "https://api.vk.com/method/utils.getLinkStats"
     path = urlparse(link).path[1:]
     payload = {
@@ -34,45 +34,34 @@ def count_clicks(token, link):
         'access_token': token,
         'v': '5.199'
     }
-    count_click = requests.post(url, data=payload, params=headers)
+    click_count = requests.post(url, data=payload, params=headers)
+    click_count.raise_for_status()
 
-    count_click.raise_for_status()
+    if 'error' in click_count.json():
+        return 'Вы ввели неправильную ссылку'
 
-    return count_click.json()
-
-
-def is_valid(link):
-    if 'error' in link:
-        return False
-    return True
+    try:
+        return click_count.json()['response']['stats'][0]['views']
+    except IndexError or KeyError:
+        return 0
 
 
 def is_shorten_link(url):
     netloc = urlparse(url).netloc
-    if 'vk.cc' in netloc:
-        return True
-    return False
+    return 'vk.cc' in netloc
 
 
 def main():
-    token = os.environ['TOKEN']
+    load_dotenv()
+    token = os.environ['TELEGRAM_TOKEN']
     entered_link = input()
 
     if is_shorten_link(entered_link):
-        click_num = count_clicks(token, entered_link)
-        if is_valid(click_num):
-            if click_num['response']['stats']:
-                print(click_num['response']['stats'][0]['views'])
-            else:
-                print(0)
-        else:
-            print('Вы ввели неправильную ссылку')
+        click_num = get_count_clicks(token, entered_link)
+        print(click_num)
     else:
-        short_link = shorten_link(token, entered_link)
-        if is_valid(short_link):
-            print(short_link['response']['short_url'])
-        else:
-            print('Вы ввели неправильную ссылку')
+        short_link = get_shorten_link(token, entered_link)
+        print(short_link)
 
 
 if __name__ == "__main__":
